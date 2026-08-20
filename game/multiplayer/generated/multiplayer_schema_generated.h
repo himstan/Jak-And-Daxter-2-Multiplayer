476ded23b98c93b07ebac10c008eda4053b93a37
himstan <hkriszto98@gmail.com>
2026-08-20 22:19:21 +0200

    fix: hover enemy spawns not getting synced

--- game/multiplayer/generated/multiplayer_schema_generated.h
+++ game/multiplayer/generated/multiplayer_schema_generated.h
@@ -43,7 +43,7 @@ inline constexpr std::array<PacketDescriptor, kPacketCount> kPackets = {{
     {14, "LOBBY_ACTION", 3, 0, 5, 268, 0, 0},
     {15, "TRAFFIC_AUTHORITY", 1, 0, 5, 32, 0, 0},
 }};
-inline constexpr std::array<EventDescriptor, 58> kEvents = {{
+inline constexpr std::array<EventDescriptor, 59> kEvents = {{
     {1, "ORB"},
     {2, "GEM"},
     {3, "BREAK"},
@@ -102,6 +102,7 @@ inline constexpr std::array<EventDescriptor, 58> kEvents = {{
     {56, "MISSION_TOMB_DAXTER_WAIT_END"},
     {57, "RACE"},
     {58, "PARTY_WIPE"},
+    {59, "HOVER_ENEMY_SPAWN"},
 }};
 
 inline const PacketDescriptor* packet_descriptor(uint8_t id) {
